9a7aab4b366ceae65084d7129444b17334e4f9ca
xuefei <xuefei@beisen.com>
2017-03-30 15:02:43 +0800

    增加一个简单位置的程序

---
+++ _serverTest/test.js
@@ -0,0 +1,28 @@
+var http = require('http')
+var url = require('url')
+var locationData=''
+
+http.createServer(function(req,res){
+	var urlParsed = url.parse(req.url,true);
+	var pathname = urlParsed.pathname
+	if(pathname=='/set'){
+		let content='';
+		req.on('data',function(chunk){
+			content += chunk;
+		})
+		req.on('end',function(){
+			locationData=content;
+			res.writeHead(200)
+			res.end('success')
+		})
+	}
+	if(pathname=='/get'){
+		if(!locationData){
+			res.writeHead(200)
+			res.end('没有位置数据')
+		}else{
+			res.writeHead(200)
+			res.end('位置信息为: '+locationData)			
+		}
+	}
+}).listen(9010,'localhost')
